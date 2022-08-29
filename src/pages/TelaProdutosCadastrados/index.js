6daae1fbfa94f09c0f4d9a63197be8bac32d1af7
GabrielMachado242 <109633533+GabrielMachado242@users.noreply.github.com>
2022-08-28 21:00:36 -0300

    corrigindo erro

--- src/pages/TelaProdutosCadastrados/index.js
+++ src/pages/TelaProdutosCadastrados/index.js
@@ -23,19 +23,18 @@ function TelaProdutos(){
                 </thead>
                 <tbody>
 
-<<<<<<< HEAD
-                      <tr key="{row.Id}">
-                      <td>{row.Npme}</td>
-                      <td>{row.Preço}</td>
-                      <td>{row.Dados}</td>
-=======
+                      <tr key="{Row.Id}" />
+                      <td>{Row.Npme}</td>
+                      <td>{Row.Preço}</td>
+                      <td>{Row.Dados}</td>
+
                 {/* <tr key="{row.Id}">
                       <td>{row.Npme}</td>
                       <td>{row.Preço}</td>
                       <td>{row.Dados}</td>
->>>>>>> 537a6823472026ab5d7b6a12a33ec9fb130d9581
+
                       
-                  </tr> */}
+                  <tr /> */}
 
                 </tbody>
             </table>
